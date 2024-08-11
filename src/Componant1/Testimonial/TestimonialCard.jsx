0e21eaf1aae48168ba500ad284b67f82b962eea4
jisan5620jk <jisank5620@gmail.com>
2024-08-11 11:53:53 +0600

    Add Component

--- src/Componant1/Testimonial/TestimonialCard.jsx
+++ src/Componant1/Testimonial/TestimonialCard.jsx
@@ -1,26 +1,44 @@
 /* eslint-disable react/prop-types */
 const TestimonialCard = ({
+  testThumb,
+  testiRating,
   testiRatingIcon,
+  testProfile,
   testiName,
   testiDesignation,
   testiDesc,
 }) => {
   return (
     <div className="relative text-center">
-      <p className="font-FiraSans sm:text-xl sm:leading-9 text-HeadingColor-0 lg:w-[75%] m-auto">
-        {testiDesc}
-      </p>
-      <ul className="flex gap-[6px] items-center justify-center mb-7 mt-6">
-        <li className="text-orange-400 text-xl">{testiRatingIcon}</li>
-        <li className="text-orange-400 text-xl">{testiRatingIcon}</li>
-        <li className="text-orange-400 text-xl">{testiRatingIcon}</li>
-        <li className="text-orange-400 text-xl">{testiRatingIcon}</li>
-        <li className="text-orange-400 text-xl">{testiRatingIcon}</li>
-      </ul>
-      <h5 className="font-FiraSans font-semibold inline-block text-HeadingColor-0 text-2xl pt-7 mb-1 relative before:absolute before:left-1/2 before:-translate-x-1/2 before:-top-0 before:w-8 before:h-[2px] before:bg-PrimaryColor-0">
-        {testiName}
-      </h5>
-      <p className="font-FiraSans text-TextColor2-0">{testiDesignation}</p>
+      <div className="Container">
+        <div>
+          <div>
+            <img src={testThumb} />
+            <h6>{testiRating}</h6>
+          </div>
+          <div>
+            <ul className="flex gap-[6px] items-center justify-center mb-7 mt-6">
+              <li className="text-orange-400 text-xl">{testiRatingIcon}</li>
+              <li className="text-orange-400 text-xl">{testiRatingIcon}</li>
+              <li className="text-orange-400 text-xl">{testiRatingIcon}</li>
+              <li className="text-orange-400 text-xl">{testiRatingIcon}</li>
+              <li className="text-orange-400 text-xl">{testiRatingIcon}</li>
+            </ul>
+            <p className="font-FiraSans sm:text-xl sm:leading-9 text-HeadingColor-0 lg:w-[75%] m-auto">
+              {testiDesc}
+            </p>
+            <div>
+              <div><img src={testProfile} /></div>
+              <div><h5 className="font-FiraSans font-semibold inline-block text-HeadingColor-0 text-2xl pt-7 mb-1 relative before:absolute before:left-1/2 before:-translate-x-1/2 before:-top-0 before:w-8 before:h-[2px] before:bg-PrimaryColor-0">
+                {testiName}
+              </h5>
+              <p className="font-FiraSans text-TextColor2-0">
+                {testiDesignation}
+              </p></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
